4d23fa7a8b353a2ad7265ed7080694dc0df5ebe5
k3nzo-dev <lboschi07@gmail.com>
2021-11-05 08:50:21 -0400

    Initial commit

---
+++ ForLoops.java
@@ -0,0 +1,70 @@
+
+/**
+ * Write a description of class ForLoops here.
+ *
+ * @author Lorenzo Boscho
+ * @version 
+ */
+public class ForLoops
+{
+    // NOTE: for all of these methods, when using Scanner to get words from the
+    //   user, use .nextLine(), NOT .next(), even if the user is entering just 
+    //   one word!!!
+    
+    //you'll want your Scanner object (and if you are usuing a Utility object)
+    //  as instance variables here, so they can be used in all the methods
+    
+    public void countVowels()
+    {
+        
+        System.out.println("NO CODE YET!");
+    }
+
+    public void reverseIt()
+    {
+        System.out.println("NO CODE YET!"); 
+    }
+
+    public void encrypt()
+    {
+        System.out.println("NO CODE YET!");  
+    }
+    
+    public void showBoard()
+    {
+        System.out.println("NO CODE YET!");   
+    }
+
+    public void showPrimes()
+    {
+        System.out.println("NO CODE YET!");
+    }
+
+    public void guessForDollars()
+    {
+        System.out.println("NO CODE YET!");
+    }
+
+    public void yourOwn()
+    {
+        System.out.println("NO CODE YET!"); 
+    }
+
+    /**
+     * THESE 3 ARE HONORS ONLY:
+     */
+    public void countConsonants()
+    {
+        System.out.println("NO CODE YET!"); 
+    }
+
+    public void showFancyBoard()
+    {
+        System.out.println("NO CODE YET!");
+    }
+
+    public void reverseByWord()
+    {
+        System.out.println("NO CODE YET!");  
+    }
+}
---
+++ ForLoopsRunner.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+/**
+ * Gives user the choice of which forloop methods to run
+ *
+ * @author Beckwith
+ * @version 1.0
+ */
+public class ForLoopsRunner
+{
+   public static void main(String[] args)
+   {
+      ForLoops f   = new ForLoops();
+      Scanner  s   = new Scanner(System.in);
+        
+      boolean done = false;   //allows multiple runs of the code
+        
+      int choice   = 0;
+        
+        //keep allowing user to choose until they quit:
+      while(!done)
+      {
+         System.out.print("\nMake a choice (1-10) 0 to quit: ");
+            
+            //List of options:
+         String list = 
+                "1. Count Vowels\n"  +
+                "2. Reverse stuff\n" +  
+                "3. Encrypt\n"       +
+                "4. Show Board\n"    +
+                "5. Show Primes\n"   +
+                "6. Guess For Dollars\n" +
+                "7. (your own here)\n"   + 
+                "8. Count Consonants\n"  + 
+                "9. Show fancy board\n"  +
+                "10. Reverse by word\n";
+      
+         System.out.print("\n" + list + "Your choice: ");
+            
+            //get choice number:
+         choice = s.nextInt();
+            
+            //clear the newline character still left in the stream:
+         s.nextLine();
+            //blank line for better readability:
+         System.out.println();
+            
+         if(choice == 0)   //user has quit
+         {
+            done = true;
+         }
+         else  //user has chosen a method:
+         {
+                //calls method associated with given choice:
+            switch(choice)
+            {
+               case 1:
+                  f.countVowels();
+                  break;
+               case 2:
+                  f.reverseIt();
+                  break;
+               case 3:
+                  f.encrypt();
+                  break;
+               case 4:
+                  f.showBoard();
+                  break;
+               case 5:
+                  f.showPrimes();
+                  break;
+               case 6:
+                  f.guessForDollars();
+                  break;
+               case 7:
+                  f.yourOwn();
+                  break;
+               case 8:
+                  f.countConsonants();
+                  break;                  
+               case 9:
+                  f.showFancyBoard();
+                  break;                 
+               case 10:
+                  f.reverseByWord();
+                  break;    
+               default:
+                  break;
+            }
+         }
+         System.out.println("Hit ENTER to go back to the main menu...");
+         s.nextLine();
+      }
+      s.close();
+      System.exit(0);  //closes console
+   }
+}
